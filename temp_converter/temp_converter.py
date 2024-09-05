7554705a674d5274bdae1459008e67e6d1ecfa77
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 14:08:37 -0400

    Update temp_converter.py
    new changes

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
-
-# process
-def c2f(c):
-    return c * 9 / 5 + 32
-
-def main(cel):
-    return c2f(cel)
-
-if __name__ == "__main__":
-    cel = 100         # input
-    print(main(cel))  # output
+HOLA THIS IS NOT CODE ANYMORE
